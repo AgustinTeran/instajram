1c3cd0e22529580343c53084f10770decc33fe32
agustinteran <agustintern@gmail.com>
2023-10-19 20:29:28 -0300

    posible solucion?

--- front/src/socket.io/index.js
+++ front/src/socket.io/index.js
@@ -9,7 +9,8 @@ export function onConect(id,setDuplicado){
   const socket = io("https://instajram-l8bb.vercel.app",{
     query: {
       id
-    }
+    },
+    withCredentials: true
   })
 
   socket.connect()
