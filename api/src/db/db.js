d5ca16d56f934c5f4426f469bb972e95b0a9672b
agustinteran <agustintern@gmail.com>
2023-10-20 13:14:27 -0300

    solo funciona en local por problema de vercel con websockets

--- api/src/db/db.js
+++ api/src/db/db.js
@@ -2,28 +2,28 @@ require("dotenv").config()
 const { Sequelize, Op } = require("sequelize");
 var {DB_USER,DB_PASS,DB_HOST,DB_NAME,DB,HOST,USER_DB,PASSWORD} = process.env
 
-// var sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}`,{logging: false})
+var sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASS}@${DB_HOST}:5432/${DB_NAME}`,{logging: false})
 
-var sequelize = new Sequelize({
-    database: `${DB}`,
-    dialect: "postgres",
-    host: `${HOST || "localhost"}`,
-    port: "5432",
-    username: `${USER_DB}`,
-    password: `${PASSWORD}`,
-    logging: false,
-    // pool: {
-    //   max: 3
-    // },
-    // dialectOptions: {
-    //   ssl: {
-    //     require: true,
-    //     rejectUnauthorized: false,
-    //   },
-    //   keepAlive: true,
-    // },
-    // ssl: true,
-  })
+// var sequelize = new Sequelize({
+//     database: `${DB}`,
+//     dialect: "postgres",
+//     host: `${HOST || "localhost"}`,
+//     port: "5432",
+//     username: `${USER_DB}`,
+//     password: `${PASSWORD}`,
+//     logging: false,
+//     pool: {
+//       max: 3
+//     },
+//     dialectOptions: {
+//       ssl: {
+//         require: true,
+//         rejectUnauthorized: false,
+//       },
+//       keepAlive: true,
+//     },
+//     ssl: true,
+//   })
 
 
 var users = require("./models/users")
