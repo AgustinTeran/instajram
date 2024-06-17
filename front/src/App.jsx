375dd8a91841675504138a02aa289e78688c0d62
agustin teran <agustintern@gmail.com>
2024-06-17 12:42:51 -0300

    redirect to the new front

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,60 +1,36 @@
-import React, { useEffect, useState } from "react";
+// import React, { useEffect, useState } from "react";
 import "./assets/styles/index.scss";
-import {useDispatch, useSelector} from "react-redux"
-import authActions from "./redux/auth/actions"
-import BigLoader from "./components/commons/bigLoader";
-import 'react-toastify/dist/ReactToastify.css';
-import UsersRoutes from "./routes/UsersRoutes";
-import { onConect } from "./socket.io";
+// import {useDispatch, useSelector} from "react-redux"
+// import authActions from "./redux/auth/actions"
+// import BigLoader from "./components/commons/bigLoader";
+// import 'react-toastify/dist/ReactToastify.css';
+// import UsersRoutes from "./routes/UsersRoutes";
+// import { onConect } from "./socket.io";
+import antes from "./assets/videos/antesWebM.webm";
+import despues from "./assets/videos/despuesWebM.webm";
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 
 
 
 
 
 function App() {
-  var [theme,setTheme] = useState(localStorage.theme || "myTheme")
-  var dispatch = useDispatch()
-
-  var {auth} = useSelector(state => state)
-
-  useEffect(() => {
-    localStorage.setItem("theme",theme)
-  },[theme])
-
-  useEffect(() => {
-    if(localStorage.getItem("token")){
-      dispatch(authActions.get())
-    } 
-  },[])
-
-  // useEffect(() => {
-  //   if(auth.user){
-  //     onConect(auth.user.id).emit("getUserOnline")
-  //   }
-  // },[auth.user]) 
-
-  useEffect(() => {
-    if(auth.error && auth.error === "No autorizado"){
-      alert("Error al autenticarse")
-      localStorage.removeItem("token")
-      window.location.replace("/")
-    }
-  },[auth.error])
-
-
-  if(localStorage.getItem("token") && !auth.user){
-    return (
-    <div data-theme={theme}>
-      <BigLoader/>
-    </div>
-    )
-  }
-  
   return (
-    <div className="min-h-screen h-full" data-theme={theme}>
-      {  
-        <UsersRoutes theme={theme} setTheme={setTheme} auth={auth}/> 
-      }
+    <div className="flex flex-col justify-center items-center gap-8 py-8">
+      <a href="https://instajram.pages.dev/" className="underline text-blue-500 text-3xl font-medium text-center hover:text-orange-500 flex gap-4 items-center">
+        Se remodeló la página para tener chats en tiempo real
+        <FaArrowUpRightFromSquare className="h-6 w-6"/>
+      </a>
+      <div className="flex gap-8 flex-wrap px-1">
+        <div className="flex-1 flex flex-col gap-3 items-center justify-center">
+          <h3 className="text-xl font-semibold">Antes</h3>
+          <video className="min-w-[350px] h-auto aspect-auto" src={antes} autoPlay muted loop></video>
+        </div>
+        <div className="flex-1 flex flex-col gap-3 items-center justify-center">
+          <h3 className="text-xl font-semibold">Ahora</h3>
+          <video className="min-w-[350px] h-auto aspect-auto" src={despues} autoPlay muted loop></video>
+        </div>
+      </div>
     </div>
   );
 }
