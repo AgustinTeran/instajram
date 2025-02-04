e9bf836652f160071e928f9771f149a6907e8687
AgustinTeran <agustintern@gmail.com>
2025-02-04 19:58:06 -0300

    this might work

--- api/index.js
+++ api/index.js
@@ -1,9 +1,13 @@
 require("pg")
 var {sequelize} = require("./src/db/db")
 var server = require("./src/server/server")
+const fs = require("fs");
 
 
 server.listen(3001,async() => {
     await sequelize.sync({alter: true})
     console.log("Listening on port 3001");
+
+    // Crear un archivo para indicar que el servidor está listo
+  fs.writeFileSync("server-ready", "OK");
 })
